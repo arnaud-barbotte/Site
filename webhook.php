b403bfd4254ef6f277c36ebf70ea3ef57566f699
ARNO <arnobarbotte@gmail.com>
2020-03-13 15:05:11 +0100

    Add files via upload

--- webhook.php
+++ webhook.php
@@ -1,3 +1,35 @@
 <?php
+// clé API du Bot à modifier
+define('TOKEN', '872097944:AAE2r2JV9zkqCLs8fGl2VNnoSwaYu3Jv3xk');
+
+// récupération des données envoyées par Telegram
 $content = file_get_contents('php://input');
-file_put_contents('message.txt', $content);
+$update = json_decode($content, true);
+
+// l'utilisateur contacte le bot
+if(preg_match('/^\/start/', $update['message']['text'])) {
+    sendMessage($update['message']['chat']['id'], 'Bonjour '.$update['message']['from']['username'].' !');
+    }
+
+// l'utilisateur envoie la commande : /gps Paris
+else if(preg_match('/^\/gps/', $update['message']['text'])) {
+    $ville = preg_replace('/^\/gps /', '', $update['message']['text']);
+    $jsonOSM = file_get_contents('https://nominatim.openstreetmap.org/search?format=json&q='.urlencode($ville));
+    $jsonOSM = json_decode($jsonOSM, true);
+    $gps = $jsonOSM[0]['display_name'].' : '.$jsonOSM[0]['lat'].','.$jsonOSM[0]['lon'];
+    sendMessage($update['message']['chat']['id'], $gps);
+    }
+
+// l'utilisateur envoie n'importe nawak
+else {
+    sendMessage($update['message']['chat']['id'], 'Je n\'ai pas compris.');
+    }
+
+// fonction qui envoie un message à l'utilisateur
+function sendMessage($chat_id, $text) {
+    $q = http_build_query([
+        'chat_id' => $chat_id,
+        'text' => $text
+        ]);
+    file_get_contents('https://api.telegram.org/bot'.TOKEN.'/sendMessage?'.$q);
+    }
